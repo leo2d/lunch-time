7c0aaf87229501057c2018ffd9c6546624afb212
Leonardo Dias <contato.leonardod@yahoo.com>
2019-05-23 00:51:10 -0300

    criacao do projeto

---
+++ LunchTime/app/src/main/java/com/leonardo/lunchtime/CustomApplication.java
@@ -0,0 +1,17 @@
+package com.leonardo.lunchtime;
+
+import com.onesignal.OneSignal;
+import android.app.Application;
+
+public class CustomApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
